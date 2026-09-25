1aa9de1e07b661b5c5afec7925e081b7a3573ae5
Sivodayan Sujeevakumar <sivodayan17@gmail.com>
2026-09-25 21:23:53 +0530

    Changed the comment to correct format in line 7

--- backend/ml_service.py
+++ backend/ml_service.py
@@ -4,7 +4,7 @@ import numpy as np
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from sklearn.linear_model import LinearRegression
-//donot change any of this
+#donot change any of this
 DB_PATH = "flood_data.db"
 DANGER_LEVEL = 50.0   
 MIN_POINTS = 5        
